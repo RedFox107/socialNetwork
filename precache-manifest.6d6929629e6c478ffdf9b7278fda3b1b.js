9eaa6b0914665e6e0a6ecb7353003639772ab6fa
RedFox107 <e.ratnikov31@gmail.com>
2020-04-10 21:52:05 +0300

    Updates

---
+++ precache-manifest.6d6929629e6c478ffdf9b7278fda3b1b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef0ba8dab236acb4d5d313784cc4487",
+    "url": "/socialNetwork/index.html"
+  },
+  {
+    "revision": "bf840cecbfe921c6f471",
+    "url": "/socialNetwork/static/css/3.7d48bf65.chunk.css"
+  },
+  {
+    "revision": "14f74066f70aa06dad8c",
+    "url": "/socialNetwork/static/css/4.5ebc5202.chunk.css"
+  },
+  {
+    "revision": "1778d02b498679b99d34",
+    "url": "/socialNetwork/static/css/main.ee6865db.chunk.css"
+  },
+  {
+    "revision": "31bb86d519fcbca3e519",
+    "url": "/socialNetwork/static/js/2.cd540ff1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/socialNetwork/static/js/2.cd540ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf840cecbfe921c6f471",
+    "url": "/socialNetwork/static/js/3.d4e36ee0.chunk.js"
+  },
+  {
+    "revision": "14f74066f70aa06dad8c",
+    "url": "/socialNetwork/static/js/4.3411e6c6.chunk.js"
+  },
+  {
+    "revision": "1778d02b498679b99d34",
+    "url": "/socialNetwork/static/js/main.3d7615cf.chunk.js"
+  },
+  {
+    "revision": "b0e1a334fe6d14d9a3c5",
+    "url": "/socialNetwork/static/js/runtime-main.1b361258.js"
+  },
+  {
+    "revision": "60d7ce3c43526a52287e5f1b94b5eb43",
+    "url": "/socialNetwork/static/media/760.60d7ce3c.gif"
+  }
+]);
